396c9a52eaef34479e3e1a828efeeb65f60d5681
Thomas Droogh <t.droogh@rijkzwaan.nl>
2020-12-12 10:27:09 +0100

    ADD: Year2015 Day02

--- AoC/Template/Day/Puzzle.cs
+++ AoC/Template/Day/Puzzle.cs
@@ -1,3 +1,4 @@
+using AoC.Util;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AoC.Template.Day
@@ -7,28 +8,50 @@ namespace AoC.Template.Day
     {
         #region Puzzle 1
 
+        private object SolvePuzzle1(string[] input)
+        {
+            return 1;
+        }
+
         [TestMethod]
         public void Setup1()
         {
+            var input = InputReader.ReadInput();
+            var result = SolvePuzzle1(input);
+            Assert.AreEqual(1, result);
         }
 
         [TestMethod]
         public void Puzzle1()
         {
+            var input = InputReader.ReadInput();
+            var result = SolvePuzzle1(input);
+            Assert.AreEqual(1, result);
         }
 
         #endregion
 
         #region Puzzle 2
 
+        private object SolvePuzzle2(string[] input)
+        {
+            return 2;
+        }
+
         [TestMethod]
         public void Setup2()
         {
+            var input = InputReader.ReadInput();
+            var result = SolvePuzzle2(input);
+            Assert.AreEqual(2, result);
         }
 
         [TestMethod]
         public void Puzzle2()
         {
+            var input = InputReader.ReadInput();
+            var result = SolvePuzzle2(input);
+            Assert.AreEqual(2, result);
         }
 
         #endregion
---
+++ AoC/Year2015/Day02/Puzzle.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Linq;
+using AoC.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AoC.Year2015.Day02
+{
+    [TestClass]
+    public class Puzzle
+    {
+        #region Puzzle 1
+
+        private int CalculateSurface(string[] input)
+        {
+            var totalSum = 0;
+
+            foreach (var line in input)
+            {
+                var split = line.Split("x");
+                var l = int.Parse(split[0]);
+                var w = int.Parse(split[1]);
+                var h = int.Parse(split[2]);
+
+                totalSum += CalculateSurface(l, w, h);
+            }
+
+            return totalSum;
+        }
+
+        private int CalculateSurface(int l, int w, int h)
+        {
+            var lw = l * w;
+            var wh = w * h;
+            var hl = h * l;
+
+            return lw * 2 + wh * 2 + hl * 2 + Math.Min(lw, Math.Min(wh, hl));
+        }
+
+        [TestMethod]
+        public void Setup1()
+        {
+            var input = InputReader.ReadInput();
+            Assert.AreEqual(101, CalculateSurface(input));
+        }
+
+        [TestMethod]
+        public void Puzzle1()
+        {
+            var input = InputReader.ReadInput();
+            Assert.AreEqual(1606483, CalculateSurface(input));
+        }
+
+        #endregion
+
+        #region Puzzle 2
+
+        private int CalculateRibbon(string[] input)
+        {
+            var totalSum = 0;
+
+            foreach (var line in input)
+            {
+                var split = line.Split("x");
+                var l = int.Parse(split[0]);
+                var w = int.Parse(split[1]);
+                var h = int.Parse(split[2]);
+
+                totalSum += CalculateRibbon(l, w, h);
+            }
+
+            return totalSum;
+        }
+
+        private int CalculateRibbon(params int[] l)
+        {
+            return l.OrderBy(x => x).Take(2).Sum() * 2 + l.Aggregate((p, n) => p * n);
+        }
+
+        [TestMethod]
+        public void Setup2()
+        {
+            var input = InputReader.ReadInput();
+            Assert.AreEqual(48, CalculateRibbon(input));
+        }
+
+        [TestMethod]
+        public void Puzzle2()
+        {
+            var input = InputReader.ReadInput();
+            Assert.AreEqual(3842356, CalculateRibbon(input));
+        }
+
+        #endregion
+    }
+}
