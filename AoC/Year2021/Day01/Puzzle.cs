e777b2b08cf3390fa32ed07299352eb611c89d63
Thomas Droogh <t.droogh@rijkzwaan.nl>
2021-12-01 08:13:57 +0100

    CHG: Simplified Y21 D01

--- AoC/Year2021/Day01/Puzzle.cs
+++ AoC/Year2021/Day01/Puzzle.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using AoC.Util;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,11 +9,11 @@ namespace AoC.Year2021.Day01
     {
         #region Puzzle 1
 
-        private object SolvePuzzle1(string[] input)
+        private static int SolvePuzzle(IReadOnlyList<int> input, int offset = 1)
         {
             var counter = 0;
-            for(var i = 0; i < input.Length - 1; i++)
-                if (int.Parse(input[i]) < int.Parse(input[i + 1]))
+            for(var i = 0; i < input.Count - offset; i++)
+                if (input[i] < input[i + offset])
                     counter++;
 
             return counter;
@@ -21,16 +22,16 @@ namespace AoC.Year2021.Day01
         [TestMethod]
         public void Setup1()
         {
-            var input = InputReader.ReadInput();
-            var result = SolvePuzzle1(input);
+            var input = InputReader.ReadIntInput();
+            var result = SolvePuzzle(input);
             Assert.AreEqual(7, result);
         }
 
         [TestMethod]
         public void Puzzle1()
         {
-            var input = InputReader.ReadInput();
-            var result = SolvePuzzle1(input);
+            var input = InputReader.ReadIntInput();
+            var result = SolvePuzzle(input);
             Assert.AreEqual(1581, result);
         }
 
@@ -38,29 +39,19 @@ namespace AoC.Year2021.Day01
 
         #region Puzzle 2
 
-        private object SolvePuzzle2(string[] input)
-        {
-            var counter = 0;
-            for(var i = 0; i < input.Length - 3; i++)
-                if (int.Parse(input[i]) < int.Parse(input[i + 3]))
-                    counter++;
-
-            return counter;
-        }
-
         [TestMethod]
         public void Setup2()
         {
-            var input = InputReader.ReadInput();
-            var result = SolvePuzzle2(input);
+            var input = InputReader.ReadIntInput();
+            var result = SolvePuzzle(input, 3);
             Assert.AreEqual(5, result);
         }
 
         [TestMethod]
         public void Puzzle2()
         {
-            var input = InputReader.ReadInput();
-            var result = SolvePuzzle2(input);
+            var input = InputReader.ReadIntInput();
+            var result = SolvePuzzle(input, 3);
             Assert.AreEqual(1618, result);
         }
 
